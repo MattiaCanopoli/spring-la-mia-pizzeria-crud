377aca3eefade681a103bf035152261515b7459f
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-08-25 19:04:20 +0200

    Create import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO pizza (price,`name`,updated_at,photo_url) VALUES (6.5,'margherita','2024-08-25 17:00:00','img/margherita.jpg'),(8,'bufala','2024-08-25 17:00:00','img/bufala.jpeg'),(5,'marinara','2024-08-25 17:00:00','img/marinara.jpg'),(7,'diavola','2024-08-25 17:00:00','img/diavola.jpg');
+
