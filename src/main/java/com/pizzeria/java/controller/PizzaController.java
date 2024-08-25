2327aff68cc249a86c63251d8d2f1525f639d9db
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-08-25 16:48:31 +0200

    Updated pizza and index controller

--- src/main/java/com/pizzeria/java/controller/PizzaController.java
+++ src/main/java/com/pizzeria/java/controller/PizzaController.java
@@ -1,5 +1,16 @@
 package com.pizzeria.java.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pizzas")
 public class PizzaController {
 
+	@GetMapping
+	public String index(Model model) {
+		return "/pizzas/index";
+	}
 }
