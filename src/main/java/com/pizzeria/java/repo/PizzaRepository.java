f976c5d74156f31241feaee342c866286086352c
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-08-25 16:57:02 +0200

    Update PizzaRepository.java

--- src/main/java/com/pizzeria/java/repo/PizzaRepository.java
+++ src/main/java/com/pizzeria/java/repo/PizzaRepository.java
@@ -1,5 +1,9 @@
 package com.pizzeria.java.repo;
 
-public interface PizzaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pizzeria.java.model.Pizza;
+
+public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
 
 }
