085c3be5918d79273f9dd80733774c54e1e1b490
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-08-25 16:57:31 +0200

    added pizzaRepo to PizzaController

--- src/main/java/com/pizzeria/java/controller/PizzaController.java
+++ src/main/java/com/pizzeria/java/controller/PizzaController.java
@@ -1,16 +1,27 @@
 package com.pizzeria.java.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pizzeria.java.model.Pizza;
+import com.pizzeria.java.repo.PizzaRepository;
+
 @Controller
 @RequestMapping("/pizzas")
 public class PizzaController {
 
+	@Autowired
+	private PizzaRepository pizzaRepo;
+
 	@GetMapping
 	public String index(Model model) {
+		List<Pizza> pizzas = pizzaRepo.findAll();
+		model.addAttribute("pizzas", pizzas);
 		return "/pizzas/index";
 	}
 }
