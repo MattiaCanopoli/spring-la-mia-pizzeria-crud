ed9c1eb2f7249752cf8c7cdddfb9b06afd6fab31
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-08-27 01:09:48 +0200

    Changed pizzas images

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO pizza (price,`name`,updated_at,photo_url) VALUES (6.5,'margherita','2024-08-25 17:00:00','img/margherita.jpg'),(8,'bufala','2024-08-25 17:00:00','img/bufala.jpeg'),(5,'marinara','2024-08-25 17:00:00','img/marinara.jpg'),(7,'diavola','2024-08-25 17:00:00','img/diavola.jpg');
+INSERT INTO pizza (price,`name`,updated_at,photo_url) VALUES (6.5,'margherita','2024-08-25 17:00:00','img/margherita.jpeg'),(8,'bufala','2024-08-25 17:00:00','img/bufala.jpeg'),(5,'acciughe','2024-08-25 17:00:00','img/acciughe.jpeg'),(7,'diavola','2024-08-25 17:00:00','img/diavola.jpeg'),(7,'prosciutto crudo','2024-08-25 17:00:00','img/crudo.jpeg'),(9,'quattro formaggi','2024-08-25 17:00:00','img/quattro-formaggi.jpeg');
 
