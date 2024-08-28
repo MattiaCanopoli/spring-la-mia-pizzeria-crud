5db0a18ae94b7af5c6c836f50daf484ca51a8d3a
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-08-29 00:47:50 +0200

    add string lorem, show mapping

--- src/main/java/com/pizzeria/java/controller/PizzaController.java
+++ src/main/java/com/pizzeria/java/controller/PizzaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pizzeria.java.model.Pizza;
@@ -18,10 +19,20 @@ public class PizzaController {
 	@Autowired
 	private PizzaRepository pizzaRepo;
 
+	private String lorem = "Lorem ipsum dolor sit amet, consectetur adipisci elit";
+
 	@GetMapping
 	public String index(Model model) {
 		List<Pizza> pizzas = pizzaRepo.findAll();
 		model.addAttribute("pizzas", pizzas);
 		return "/pizzas/index";
+	}
+
+	@GetMapping("/{id}")
+	public String show(@PathVariable("id") Integer id, Model model) {
+		model.addAttribute("thisPizza", pizzaRepo.findById(id).get());
+		model.addAttribute("pizzas", pizzaRepo.findAll());
+		model.addAttribute("lorem", lorem);
+		return "/pizzas/show";
 	}
 }
