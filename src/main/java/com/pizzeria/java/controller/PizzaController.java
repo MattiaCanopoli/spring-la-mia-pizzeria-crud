27b3a16431561827db3343733e6ffade3842371d
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-09-01 00:57:34 +0200

    add: finByNameContains()

--- src/main/java/com/pizzeria/java/controller/PizzaController.java
+++ src/main/java/com/pizzeria/java/controller/PizzaController.java
@@ -3,11 +3,13 @@ package com.pizzeria.java.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.pizzeria.java.model.Pizza;
 import com.pizzeria.java.repo.PizzaRepository;
@@ -22,8 +24,16 @@ public class PizzaController {
 	private String lorem = "Lorem ipsum dolor sit amet, consectetur adipisci elit";
 
 	@GetMapping
-	public String index(Model model) {
-		List<Pizza> pizzas = pizzaRepo.findAll();
+	public String index(Model model, @RequestParam(name = "name", required = false) String name) {
+
+		List<Pizza> pizzas;
+
+		if (name != null && !name.isEmpty()) {
+
+			pizzas = pizzaRepo.findByNameContains(name);
+		} else {
+			pizzas = pizzaRepo.findAll(Sort.by("name"));
+		}
 		model.addAttribute("pizzas", pizzas);
 		return "/pizzas/index";
 	}
